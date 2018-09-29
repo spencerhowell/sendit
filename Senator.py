5a682547c4890deff4a3b3ac913d06ec8d578812
msawchuk <42722709+msawchuk@users.noreply.github.com>
2018-09-29 14:24:17 -0400

    thesenate

--- Senator.py
+++ Senator.py
@@ -1,26 +1,13 @@
+import csv
+
 class Senator:
-    def __init__(self, name, state, email):
+    def __init__(self, name, state, contact_me, party, pic, next_senator=None):
         self.name = name
         self.state = state
-        self.email = email
-
-
-s1 = Senator("Corker")
-print(s1.name)
-
-class Node:
-    def __init__(self, senator=None, next_senator=None):
-        self.senator = senator
+        self.contact_me = contact_me
+        self.party = party
+        self.pic = pic
         self.next_senator = next_senator
-
-    def getSenator(self):
-        return self.senator
-
-    def getNext(self):
-        return self.next_senator
-
-    def setNext(self, next):
-        self.next_senator = next
 
 class Senate:
     def __init__(self, size = 100, firstSenator = None):
@@ -29,8 +16,8 @@ class Senate:
 
     def moveToLastSenator(self):
         currentSenator = self.firstSenator
-        while(currentSenator != None):
-            currentSenator = currentSenator.getNext
+        while(currentSenator.next_senator != None):
+            currentSenator = currentSenator.next_senator
         return currentSenator
 
 
@@ -39,16 +26,17 @@ class Senate:
             self.firstSenator = senator
         else:
             lastSenator = self.moveToLastSenator()
-            lastSenator.setNext(senator)
-
+            lastSenator.next_senator = senator
 
     def findSentaors(self, state):
         foundSenators = 0
         currentSenator = self.firstSenator
         thisState = Senate(2)
-        while (currentSenator.next_Senator != None & foundSenators < 2 ):
+        while ((currentSenator.next_senator != None) & (foundSenators < 2)):
             if(currentSenator.state == state):
                 thisState.addSenator(currentSenator)
+                foundSenators = foundSenators + 1
+            currentSenator = currentSenator.next_senator
         return thisState
 
     def getSenatorName(self, senatorName):
@@ -62,5 +50,21 @@ class Senate:
     def getSenatorNum(self, senatorNum):
         currentSenator = self.firstSenator
         for x in range(1, senatorNum):
-            currentSenator = currentSenator.getNext
+            currentSenator = currentSenator.next_senator
         return currentSenator
+
+ourSenate = Senate(100, None)
+with open('us-senate.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        name = row['last_name']
+        state = row['state_name']
+        contact_me = row['contact_page']
+        party = row['party']
+        pic = row['photo_url']
+        senator1 = Senator(name, state, contact_me, party, pic)
+        ourSenate.addSenator(senator1)
+Georgia = ourSenate.findSentaors("Georgia")
+print(Georgia.getSenatorNum(1).name)
+print(Georgia.getSenatorNum(2).name)
+print(Georgia.size)
